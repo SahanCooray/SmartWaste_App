d71a53936abd91325e4fa95bd58bb1c897b7dff8
Tumasha Deshan <tumashadeshan@gmail.com>
2024-03-23 08:13:56 +0530

    Added leaderboard

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -5,8 +5,28 @@ class LeaderBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Leaderboard'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Leaderboard",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            Center(
+              child: Image(
+                image: AssetImage("assets/lb.png"),
+                fit: BoxFit.fill,
+                width: 350,
+                height: 600,
+              ),
+            ),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
     );
   }
 }
